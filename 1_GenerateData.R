52ffec125651a67df8703d7910ece593c1c58a3e
DannyGsGit <danny.godbout@gmail.com>
2016-10-15 21:44:07 -0700

    Added commenting.

--- 1_GenerateData.R
+++ 1_GenerateData.R
@@ -1,5 +1,9 @@
 # Use this script to create synthetic data
 
+#----------------------------
+#### Generate sales data ####
+#----------------------------
+
 
 ## Generate a baseline monthly order rate
 vehicle.sales <- rnorm(n = 12 * 30, mean = 400, sd = 40)
@@ -8,7 +12,7 @@ vehicle.sales <- rnorm(n = 12 * 30, mean = 400, sd = 40)
 season.1 <- .05 * sinpi((1:length(vehicle.sales)) / 12) + 1
 vehicle.sales <- vehicle.sales * season.1
 
-## Add start of production ramp-up
+## Add start/end of production ramps
 start <- rep(0, times = 12 * 1)
 ramp.up <- seq(from = 0, to = 1, by = 0.04)
 ramp.down <- seq(from = 1, to = 0, by = -0.05)
@@ -22,3 +26,16 @@ vehicle.sales <- vehicle.sales * production.ramps
 
 ## Plot dataset
 plot(vehicle.sales, type = "l")
+
+
+#----------------------------------
+#### Generate replacement data ####
+#----------------------------------
+
+# A normal distribution for replacement rate due to damage
+
+# A weibull distribution for replacement due to wear
+
+
+
+
